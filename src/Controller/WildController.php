f6afe4fc1ff0eb4ac19386b1bb4f6d0459ac813a
korede lacici <lafical@yahoo.fr>
2019-11-22 17:05:02 +0100

    création page d'accueil

---
+++ src/Controller/WildController.php
@@ -0,0 +1,23 @@
+<?php
+//src/Controller/WildController
+
+namespace App\Controller;
+
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WildController extends AbstractController
+{
+    /**
+     * @Route("/wild", name="wild_index")
+     */
+    public function index() :Response
+    {
+        return $this->render('wild/index.html.twig', [
+            'website' => 'Wild Séries',
+        ]);
+    }
+}
