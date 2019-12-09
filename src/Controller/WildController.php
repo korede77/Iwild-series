ff8975a69b0a826ea1fddc1b6c0c2cb99a991d83
korede lacici <lafical@yahoo.fr>
2019-12-09 16:46:33 +0100

    correction showEpisode

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -9,12 +9,14 @@ use App\Entity\Category;
 use App\Entity\Episode;
 use App\Entity\Program;
 use App\Entity\Season;
+use App\Form\ProgramSearchType;
 use App\Repository\CategoryRepository;
 use App\Repository\SeasonRepository;
 use Doctrine\ORM\Query\AST\OrderByItem;
 use Doctrine\ORM\Query\Expr\OrderBy;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -31,6 +33,11 @@ class WildController extends AbstractController
      */
     public function index() :Response
     {
+        $form = $this->createForm(ProgramSearchType::class,
+            null,
+            ['method' => Request::METHOD_GET]
+        );
+
         $programs = $this->getDoctrine()
             ->getRepository(Program::class)
             ->findAll();
@@ -41,6 +48,7 @@ class WildController extends AbstractController
         }
         return $this->render('wild/index.html.twig', [
             'programs' => $programs,
+            'form' => $form->createView(),
         ]);
     }
     /**
@@ -163,11 +171,11 @@ class WildController extends AbstractController
         ]);
     }
     /**
-     * @param integer $category
-     * @Route("/category/{category}/episode/{episode}", name="episode")
+     * @param integer $id
+     * @Route("/episode/{id}", name="episode")
      * @return Response
      */
-    public function showEpisode(Category $category, Episode $episode):Response
+    public function showEpisode( Episode $episode):Response
     {
         $season = $episode->getSeason();
         $program = $season->getProgram();
@@ -177,5 +185,6 @@ class WildController extends AbstractController
             'episode' => $episode,
         ]);
     }
+
 
 }
